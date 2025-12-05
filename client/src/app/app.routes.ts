1a4bc9a2de8efb35b6dff4a6dfec65e09edbe1a2
DanielFloresTamayo <danielflorestamayo@gmail.com>
2025-12-04 19:02:17 -0500

    Primer prototipo del proyecto

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,28 +1,38 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './login/login.component';
-import { RegistrarComponent } from './registrar-asignatura/registrar.component';
-import { GestionUsuarioComponent } from './gestion-usuario/gestion-usuario.component';
 import { RegistroParticipanteComponent } from './registro-participante/registro-participante.component';
 import { RegistroTutorComponent } from './registro-tutor/registro-tutor.component';
 import { TUTOR_DASHBOARD_ROUTES } from './tutor-dashboard/tutor-dashboard-routing';
-import { TutorProfileComponent } from './tutor-dashboard/tutor-profile/tutor-profile.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PARTICIPANTE_DASHBOARD_ROUTES } from './participante-dashboard/participante-dashboard-routing';
+
 
 const appRoutes = [
-{ path: 'tutor', children: TUTOR_DASHBOARD_ROUTES }, // Carga las rutas del dashboard del tutor
-{ path: '', redirectTo: 'tutor/perfil', pathMatch: 'full' }, // Redirección por defecto
+  { path: 'tutor', children: TUTOR_DASHBOARD_ROUTES }, // Carga las rutas del dashboard del tutor
+  { path: '', redirectTo: 'tutor/perfil', pathMatch: 'full' }, // Redirección por defecto
 
-  //  {path: "app-login", component: LoginComponent, pathMatch: "full"},
-  //   {path: "app-registrar", component: RegistrarComponent, pathMatch: "full"},
+
 ];
 
 export const routes: Routes = [
-  // path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "registrar", component: RegistrarComponent, pathMatch: "full" },
-  { path: "gestion-usuario", component: GestionUsuarioComponent, pathMatch: "full" },
-  { path: "registro-participante", component: RegistroParticipanteComponent, pathMatch: "full" },
-  { path: "registro-tutor", component: RegistroTutorComponent, pathMatch: "full" },
+
+  //{ path: 'tutor/perfil', loadComponent: () => import('./tutor-dashboard/tutor-profile/tutor-profile.component').then(m => m.TutorProfileComponent), canActivate: [AuthGuard] },
+  { path: 'registro-participante', component: RegistroParticipanteComponent, pathMatch: 'full' },
+  { path: 'registro-tutor', component: RegistroTutorComponent, pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
-  { path: 'tutor/perfil', loadComponent: () => import('./tutor-dashboard//tutor-profile/tutor-profile.component').then(m => m.TutorProfileComponent) }, 
+
+  // 🔹 Aquí cargamos TODO el dashboard del tutor (con sidebar y sus subrutas)
+  {
+    path: 'tutor',
+    canActivate: [AuthGuard],
+    children: TUTOR_DASHBOARD_ROUTES
+  },
+
+  {
+    path: 'participante',
+    canActivate: [AuthGuard],
+    children: PARTICIPANTE_DASHBOARD_ROUTES
+  },
+
 ];
 
